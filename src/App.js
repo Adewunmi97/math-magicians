b06980cbe0417412510494ee84d4730113735b0a
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2021-10-23 22:15:43 +0100

    remove full website from refactoring branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
-import Calc from './components/pages/Calc';
-import Quote from './components/pages/Quote';
+import Calculator from './components/Calculator';
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Router>
-          <Navbar />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/calc" component={Calc} />
-            <Route path="/quote" component={Quote} />
-          </Switch>
-        </Router>
-      </>
+      <Calculator />
     );
   }
 }
