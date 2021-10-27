6a7e2852f2bafc1f47f1aa06d050255426d5c181
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2021-10-27 14:48:29 +0100

    add test for calculator.js

---
+++ src/logic/test/calculate.test.js
@@ -0,0 +1,31 @@
+import calculate from '../calculate'
+
+describe ('calculate', () => {
+    test ('AC button', () => {
+        expect(calculate({}, 'AC')).toEqual({
+            total: null,
+            next: null,
+            operation: null,
+          });
+    });
+
+    test ('return empty array', () => {
+        expect(calculate({next: '0'}, '0')).toEqual({});
+    });
+
+    test ('return users input', () => {
+        expect(calculate({operation: '+'}, '2')).toEqual({
+            next: '2',
+            operation: '+',
+          });
+    });
+
+    test ('return users input with decimal', () => {
+        expect(calculate({operation: '.'}, '2')).toEqual({
+            next: '2',
+            operation: '.',
+          });
+    });
+
+
+});
