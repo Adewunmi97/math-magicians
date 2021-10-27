ed1ec3cd6e47f15d74309909dccd122d8538c72f
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2021-10-27 14:00:59 +0100

    add test for operate.js

---
+++ src/logic/test/operate.test.js
@@ -0,0 +1,23 @@
+import operate from '../operate'
+
+describe ('operate', () => {
+    test ('add numbers', () => {
+        expect(operate(2, 2, '+')).toBe('4');
+    });
+
+    test ('subtract numbers', () => {
+        expect(operate(4, 2, '-')).toBe('2');
+    });
+
+    test ('multiply numbers', () => {
+        expect(operate(2, 2, 'x')).toBe('4');
+    });
+
+    test ('divide numbers', () => {
+        expect(operate(2, 2, '÷')).toBe('1');
+    });
+
+    test ('remainder numbers', () => {
+        expect(operate(4, 2, '%')).toBe('0');
+    });
+});
