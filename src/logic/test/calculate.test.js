a6a21c05d0d6df43e38d3f37b058c420bc0c2135
brianorora <brianorora@gmail.com>
2021-10-28 17:04:56 +0300

    Fix: stylelint error

--- src/logic/test/calculate.test.js
+++ src/logic/test/calculate.test.js
@@ -1,31 +1,29 @@
-import calculate from '../calculate'
+import calculate from '../calculate';
 
-describe ('calculate', () => {
-    test ('AC button', () => {
-        expect(calculate({}, 'AC')).toEqual({
-            total: null,
-            next: null,
-            operation: null,
-          });
+describe('calculate', () => {
+  test('AC button', () => {
+    expect(calculate({}, 'AC')).toEqual({
+      total: null,
+      next: null,
+      operation: null,
     });
+  });
 
-    test ('return empty array', () => {
-        expect(calculate({next: '0'}, '0')).toEqual({});
+  test('return empty array', () => {
+    expect(calculate({ next: '0' }, '0')).toEqual({});
+  });
+
+  test('return users input', () => {
+    expect(calculate({ operation: '+' }, '2')).toEqual({
+      next: '2',
+      operation: '+',
     });
+  });
 
-    test ('return users input', () => {
-        expect(calculate({operation: '+'}, '2')).toEqual({
-            next: '2',
-            operation: '+',
-          });
+  test('return users input with decimal', () => {
+    expect(calculate({ operation: '.' }, '2')).toEqual({
+      next: '2',
+      operation: '.',
     });
-
-    test ('return users input with decimal', () => {
-        expect(calculate({operation: '.'}, '2')).toEqual({
-            next: '2',
-            operation: '.',
-          });
-    });
-
-
+  });
 });
--- src/logic/test/operate.test.js
+++ src/logic/test/operate.test.js
@@ -1,23 +1,23 @@
-import operate from '../operate'
+import operate from '../operate';
 
-describe ('operate', () => {
-    test ('add numbers', () => {
-        expect(operate(2, 2, '+')).toBe('4');
-    });
+describe('operate', () => {
+  test('add numbers', () => {
+    expect(operate(2, 2, '+')).toBe('4');
+  });
 
-    test ('subtract numbers', () => {
-        expect(operate(4, 2, '-')).toBe('2');
-    });
+  test('subtract numbers', () => {
+    expect(operate(4, 2, '-')).toBe('2');
+  });
 
-    test ('multiply numbers', () => {
-        expect(operate(2, 2, 'x')).toBe('4');
-    });
+  test('multiply numbers', () => {
+    expect(operate(2, 2, 'x')).toBe('4');
+  });
 
-    test ('divide numbers', () => {
-        expect(operate(2, 2, '÷')).toBe('1');
-    });
+  test('divide numbers', () => {
+    expect(operate(2, 2, '÷')).toBe('1');
+  });
 
-    test ('remainder numbers', () => {
-        expect(operate(4, 2, '%')).toBe('0');
-    });
+  test('remainder numbers', () => {
+    expect(operate(4, 2, '%')).toBe('0');
+  });
 });
